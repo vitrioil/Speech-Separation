1c434cee4f7268822ac75f15fc5e95219043f5d7
vitrioil <opm249@gmail.com>
2020-01-20 20:12:09 +0530

    MTCNN face detect bug

--- archive.sh
+++ archive.sh
@@ -2,4 +2,4 @@
 
 #zip downloaded dataset
 
-zip dataset.zip data/audio_set/audio/* data/train/embed/* data/train/audio/* data/train/mixed/* data/input_df.csv
+zip dataset.zip data/audio_set/audio/* data/train/embed/* data/train/audio/* data/train/mixed/* src/more_train.csv src/more_val.csv
