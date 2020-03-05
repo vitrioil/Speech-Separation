763ba1255011bfbdc30866909a387e62225322c9
vitrioil <opm249@gmail.com>
2020-03-05 11:23:39 +0530

    added bandpass postprocessing, and audio_mixer_generator handling combination size

--- setup.sh
+++ setup.sh
@@ -4,9 +4,7 @@
 
 mkdir -p data/audio_set/audio
 mkdir -p data/audio_visual/
-mkdir -p data/train/audio/
-mkdir -p data/train/mixed
-mkdir -p data/train/embed
+mkdir -p data/train/{audio,mixed,embed,spec}/
 
 #Install dependencies
 pip install -r requirements.txt
